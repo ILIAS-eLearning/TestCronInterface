e318cab2e2f847a0c34282a07474866da3aab09a
mjansen <mjansen@databay.de>
2023-09-19 15:25:36 +0200

    Prepare ILIAS 10 support

--- plugin.php
+++ plugin.php
@@ -7,7 +7,7 @@
  */
 $id = 'tstcroninterface';
 $version = '1.0';
-$ilias_min_version = '9.0';
-$ilias_max_version = '9.999';
+$ilias_min_version = '10.0';
+$ilias_max_version = '10.999';
 $responsible = 'Michael Jansen';
 $responsible_mail = 'mjansen@databay.de';
